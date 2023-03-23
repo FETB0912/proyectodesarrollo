9e19ee90b5595ad50652329efda46fc7e5a9a20c
Eduarcrz1618 <eduarcruz1618@gmail.com>
2023-03-23 00:25:35 -0600

    models

--- app/Models/service.php
+++ app/Models/service.php
@@ -8,10 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
-
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'services';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'price',
-       
     ];
 }
